fafd89aba11aee50689eb8f7cfde366d5ccff0f4
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-08-24 09:45:55 -0400

    Camio en puerto de escucha y app.get

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('🛠️ Backend de Nesty Jewelry está activo');
+});
+
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`✅ Servidor corriendo en puerto ${PORT}`);
 });
