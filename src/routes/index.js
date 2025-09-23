df04d566097d9ca15775a4a148c4d71c9d89d0d5
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-09-23 12:22:43 -0400

    HHHHHH

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import authRoutes from "./authRoutes.js";
 import productRoutes from "./productRoutes.js";
 import cartRoutes from "./cartRoutes.js";
-import paymentRoutes from "./paymentRoutes.js";
 import wishlistRoutes from "./wishlistRoutes.js";
 import orderRoutes from "./orderRoutes.js";
 
@@ -11,7 +10,6 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/products", productRoutes);
 router.use("/cart", cartRoutes);
-router.use("/payments", paymentRoutes);
 router.use("/wishlist", wishlistRoutes);
 router.use("/orders", orderRoutes);
 
